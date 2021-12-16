1ced72f4553cb382064a17c274da9f19becfc5e0
Valentino Martinez <93661868+ValentinoMartz@users.noreply.github.com>
2021-12-16 18:06:25 -0300

    HW III DONE

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -26,11 +26,12 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
-  arrayAumento = [];
+   arrayAumento = [];
   for(let i = 0; i < array.length; i++){
     arrayAumento[i] = array[i] + 1;
   }
-  return arrayAumento;
+  return arrayAumento; 
+  
 }
 
 
@@ -120,17 +121,12 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
 // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
-  let argMult = 1;
-  if(arguments.length < 1){
-    return 0;
-  }else if(arguments.length == 1){
-    return arguments[0];
-  }else{
-    for(let i = 0; i < arguments.length; i++){
-      argMult *= arguments[i];
-    }
-    return argMult;
+  argnew = 1;
+  if(arguments.length === 0)return 0
+  for(i = 0; i < arguments.length; i++){
+    argnew *=arguments[i];
   }
+  return argnew;
 }
 
 
@@ -138,13 +134,11 @@ function multiplicarArgumentos() {
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-  cont = 0;
+  let cont = 0
   for(let i = 0; i < arreglo.length; i++){
-    if(arreglo[i] > 18){
-      cont++;
-    }
+    if(arreglo[i] > 18) cont++;
   }
-  return cont;
+  return cont
 }
 
 
@@ -164,12 +158,19 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
-   let primerValor = n.toString();
+    let nueve = n.toString()
+  if(nueve.charAt(0) == '9'){
+      return true
+    } else{
+      return false;
+    }
+
+    /* let primerValor = n.toString();
   if(primerValor.charAt(0) === "9"){
     return true;
   }else{
   return false;
-  } 
+  }*/
   
 }
 
@@ -177,14 +178,14 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí 
-  for(let i = 0; i < arreglo.length - 1; i++){
-    if(arreglo[i] !== arreglo[i+1]){
+  let aux = arreglo[0]
+  for(let i = 1; i < arreglo.length; i++){
+    if(aux !== arreglo[i]){
       return false
     } 
-  }  
-  return true;
-} 
-
+  }
+  return true
+}
 
 
 function mesesDelAño(array) {
@@ -192,18 +193,14 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
-  nuevoArray = [];
-  for(i = 0; i < array.length; i++){
-    if(array[i] == "Enero" ||array[i] == "Marzo"|| array[i] == "Noviembre"){
-      nuevoArray.push(array[i]); 
-      }
-      //Me di cuenta que si los meses se repiten y cumplen la condicion se guardan de nuevo
-     //como no se si esta bien o no, lo dejo sin modificarlo
+  let meses = [];
+  for(let i = 0; i < array.length; i++){
+    if(array[i] === "Enero" || array[i] === "Marzo" || array[i] === "Noviembre"){
+      meses.push(array[i]);
+    }
   }
-  if(nuevoArray.length < 3 ){
-    return "No se encontraron los meses pedidos";
-  }
-  return nuevoArray;
+  if(meses.length < 3) return "No se encontraron los meses pedidos"
+  else return meses;
 }
 
 
@@ -228,23 +225,20 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
-  let array = []; 
-  
-  for(var i = 1; i <= 10; i++){
-    numero += 2;
-    if(numero == i){
-      break;
-    }else{
-      array.push(numero);
-    }
-  }
-  if(i < 10 ){
-    return "Se interrumpió la ejecución"
-  }else{
-    return array;
-  }
-} 
 
+  let nuevoArray = [];
+  let suma = numero;
+  for(let i = 0; i <10; i++){
+     suma += 2;
+    if(suma === i) break;
+    else nuevoArray.push(suma);
+  }
+  if(numero < 10) return 'Se interrumpió la ejecución';
+  
+  return nuevoArray;
+  
+  
+}
  function continueStatement(numero) {
   //Iterar en un bucle aumentando en 2 el numero recibido hasta un límite de 10 veces.
   //Guardar cada nuevo valor en un array.    
